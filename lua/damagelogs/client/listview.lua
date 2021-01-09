9a5a1a9f5f2b658bb0019a7a7cec0623d80fc5f2
Michael Hawkins <BadgerCode@users.noreply.github.com>
2021-01-09 16:32:22 +0000

    Handles TTT gamemode initialising after damage logs for role colours

--- lua/damagelogs/client/listview.lua
+++ lua/damagelogs/client/listview.lua
@@ -201,17 +201,23 @@ function Damagelog:SetRolesListView(listview, tbl)
     end
 end
 
--- Defined in terrortown/gamemode/shared.lua
--- ROLE_INNOCENT  = 0
--- ROLE_TRAITOR   = 1
--- ROLE_DETECTIVE = 2
+local role_colors = { }
 
-local role_colors = {
-    [ROLE_INNOCENT] = Color(0, 200, 0),
-    [ROLE_TRAITOR] = Color(200, 0, 0),
-    [ROLE_DETECTIVE] = Color(0, 0, 200),
-    ["disconnected"] = Color(0, 0, 0)
-}
+-- We need to wait for the TTT gamemode to init to ensure the ROLE_XXX values are defined
+hook.Remove("Initialize", "damagelogs_gminit_rolecolours")
+hook.Add("Initialize", "damagelogs_gminit_rolecolours", function()
+    -- Defined in terrortown/gamemode/shared.lua
+    -- ROLE_INNOCENT  = 0
+    -- ROLE_TRAITOR   = 1
+    -- ROLE_DETECTIVE = 2
+
+    role_colors = {
+        [ROLE_INNOCENT] = Color(0, 200, 0),
+        [ROLE_TRAITOR] = Color(200, 0, 0),
+        [ROLE_DETECTIVE] = Color(0, 0, 200),
+        ["disconnected"] = Color(0, 0, 0)
+    }
+end)
 
 function Damagelog:AddRoleLine(listview, nick, role)
     if role ~= -3 then
