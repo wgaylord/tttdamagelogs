32210e093746899af76c892107ff3d641bea5967
Michael Hawkins <BadgerCode@users.noreply.github.com>
2023-05-13 20:24:57 +0100

    Bump version to 3.6.1

--- lua/autorun/damagelog_autorun.lua
+++ lua/autorun/damagelog_autorun.lua
@@ -1,6 +1,6 @@
 -- if engine.ActiveGamemode() == 'terrortown' then
 Damagelog = Damagelog or {}
-Damagelog.VERSION = "3.6.0"
+Damagelog.VERSION = "3.6.1"
 
 if not file.Exists("damagelog", "DATA") then
     file.CreateDir("damagelog")
