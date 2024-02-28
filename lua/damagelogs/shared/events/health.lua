36fce35db8fd2e741cfa73f72b5b7c4f2de50094
Michael Hawkins <BadgerCode@users.noreply.github.com>
2024-02-28 23:14:22 +0000

    Reduce nesting

--- lua/damagelogs/shared/events/health.lua
+++ lua/damagelogs/shared/events/health.lua
@@ -11,28 +11,29 @@ local event = {}
 event.Type = "HEAL"
 
 function event:TTTPlayerUsedHealthStation(ply, ent, healed)
-    if not (ply.IsGhost and ply:IsGhost()) and ply:IsPlayer() then
-        if usages[ply:SteamID()] == nil then
-            local timername = "HealTimer_" .. tostring(ply:SteamID())
+    if (ply.IsGhost and ply:IsGhost()) or not ply:IsPlayer() then return end
 
-            timer.Create(timername, 5, 0, function()
-                if not IsValid(ply) then
-                    timer.Remove(timername)
-                elseif usages[ply:SteamID()] > 0 then
-                    self:Store(ply, ent, usages[ply:SteamID()])
-                    usages[ply:SteamID()] = 0
-                else
-                    usages[ply:SteamID()] = nil
-                    self:RemoveTimer(ply:SteamID())
-                end
-            end)
+    if usages[ply:SteamID()] ~= nil then
+        usages[ply:SteamID()] = usages[ply:SteamID()] + healed
+        return
+    end
 
-            self:Store(ply, ent, healed)
+    local timername = "HealTimer_" .. tostring(ply:SteamID())
+
+    timer.Create(timername, 5, 0, function()
+        if not IsValid(ply) then
+            timer.Remove(timername)
+        elseif usages[ply:SteamID()] > 0 then
+            self:Store(ply, ent, usages[ply:SteamID()])
             usages[ply:SteamID()] = 0
         else
-            usages[ply:SteamID()] = usages[ply:SteamID()] + healed
+            usages[ply:SteamID()] = nil
+            self:RemoveTimer(ply:SteamID())
         end
-    end
+    end)
+
+    self:Store(ply, ent, healed)
+    usages[ply:SteamID()] = 0
 end
 
 function event:Store(ply, ent, healed)
