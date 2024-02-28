21b94b01e15f755bdc31ba31e6d1bacf676d9496
Michael Hawkins <BadgerCode@users.noreply.github.com>
2024-02-28 23:32:48 +0000

    Supports TTT2's GetOriginator alternative to placer of health station

--- lua/damagelogs/shared/events/health.lua
+++ lua/damagelogs/shared/events/health.lua
@@ -18,13 +18,17 @@ function event:TTTPlayerUsedHealthStation(ply, ent, healed)
         return
     end
 
+    local healthStationOwner = (ent.GetPlacer and ent:GetPlacer()) -- Vanilla TTT
+        or (ent.GetOriginator and ent:GetOriginator()) -- TTT2
+        or nil
+
     local timername = "HealTimer_" .. tostring(ply:SteamID())
 
     timer.Create(timername, 5, 0, function()
         if not IsValid(ply) then
             timer.Remove(timername)
         elseif usages[ply:SteamID()] > 0 then
-            self:Store(ply, ent, usages[ply:SteamID()])
+            self:Store(ply, healthStationOwner, usages[ply:SteamID()])
             usages[ply:SteamID()] = 0
         else
             usages[ply:SteamID()] = nil
@@ -32,25 +36,18 @@ function event:TTTPlayerUsedHealthStation(ply, ent, healed)
         end
     end)
 
-    self:Store(ply, ent, healed)
+    self:Store(ply, healthStationOwner, healed)
     usages[ply:SteamID()] = 0
 end
 
-function event:Store(ply, ent, healed)
+function event:Store(ply, healthStationOwner, healed)
+    local isOwnerValid = IsValid(healthStationOwner)
     local tbl = {
         [1] = ply:GetDamagelogID(),
-        [2] = healed
+        [2] = healed,
+        [3] = isOwnerValid,
+        [4] = isOwnerValid and healthStationOwner:GetDamagelogID() or nil
     }
-
-    local placer = ent:GetPlacer()
-    local validOwner = IsValid(placer)
-
-    if validOwner then
-        table.insert(tbl, true)
-        table.insert(tbl, placer:GetDamagelogID())
-    else
-        table.insert(tbl, false)
-    end
 
     self.CallEvent(tbl)
 end
